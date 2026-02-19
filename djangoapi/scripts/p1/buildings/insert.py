b7e9868eee314d026b8d9945c22977d5416a1dd5
Adolfo Piedra Mora <aepiemor@topo.upv.es>
2026-02-19 22:48:28 +0100

    Mis cambios en la API y configuracion de base de datos

---
+++ djangoapi/scripts/p1/buildings/insert.py
@@ -0,0 +1,29 @@
+import psycopg
+from myLib import p1Settings
+def connect():
+    conn = psycopg.connect(
+        dbname=p1Settings.POSTGRES_DB,
+        user=p1Settings.POSTGRES_USER,
+        password=p1Settings.POSTGRES_PASSWORD,
+        host=p1Settings.POSTGRES_HOST,
+        port=p1Settings.POSTGRES_PORT
+                        )
+    print("Connected")
+    return conn
+    #conn.close()
+
+def insert():
+    conn=connect()
+    cur=conn.cursor()
+    cons="""
+            INSERT INTO d.buildings(description, area,geom)
+            VALUES (%s,%s,st_geometryFromText(%s,25830)) 
+            RETURNING ID
+        """
+    cur.execute(cons,['My first building',100,'POLYGON ((0 0,100 0,100 100, 0 100, 0 0))'])
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("Inserted")
+
